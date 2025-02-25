89a8b0c42b1fdf90442c5125b903d5f4d362bf4d
jonhumphreys <93622145+jonhumphreys@users.noreply.github.com>
2025-02-25 15:43:57 -0800

    After 2/25 class

--- AdventureS25/CommandProcessor.cs
+++ AdventureS25/CommandProcessor.cs
@@ -2,6 +2,15 @@
 
 public static class CommandProcessor
 {
+    public static List<string> Verbs = new List<string>
+        {"go", "eat"};
+    
+    public static List<string> StandaloneVerbs = new List<string>
+        {"exit", "inventory", "look"};
+    
+    public static List<string> Nouns = new List<string>
+        {"bagel", "apple", "beer"};
+    
     public static void Process()
     {
         // get input
@@ -14,11 +23,60 @@ public static class CommandProcessor
         Console.WriteLine("Noun: [" + command.Noun + "]");
         
         // make sure we have the words in our vocabulary
+        bool isValid = IsValid(command);
 
         // do stuff with the command
+        Console.WriteLine("isValid = " + isValid);
+    }
 
+    private static bool IsValid(Command command)
+    {
+        bool isValid = false;
+        
+        if (IsVerb(command.Verb) || IsStandaloneVerb(command.Verb))
+        {
+            if (IsStandaloneVerb(command.Verb))
+            {
+                isValid = true;
+            }
+            else if (IsNoun(command.Noun))
+            {
+                isValid = true;
+            }
+        }
+            
+        return isValid;
     }
     
+    private static bool IsNoun(string commandNoun)
+    {
+        if (Nouns.Contains(commandNoun))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private static bool IsStandaloneVerb(string commandVerb)
+    {
+        if (StandaloneVerbs.Contains(commandVerb))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private static bool IsVerb(string commandVerb)
+    {
+        if (Verbs.Contains(commandVerb))
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
+
     public static string GetInput()
     {
         Console.Write("> ");
@@ -33,6 +91,11 @@ public static class CommandProcessor
         
         // breaK the input into individual words
         List<string> words = input.Split(' ').ToList();
+
+        if (input == "")
+        {
+            words = new List<string>();
+        }
         
         Command command = new Command();
         
